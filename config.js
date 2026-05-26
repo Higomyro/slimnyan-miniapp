4efff9068694a5240ce3a61685f1c6e1f4b7d759
Higomyro <higomyro@gmail.com>
2026-05-26 17:04:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 const CONFIG = {
-    API_URL: 'https://slightly-projector-rigging.ngrok-free.dev/api/command'
+    API_URL: 'https://slightly-projector-rigging.ngrok-free.dev/api/command',
+    FALLBACK_USER_ID: 7617765563
 };
